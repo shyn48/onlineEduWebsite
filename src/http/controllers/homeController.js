88a7ed366aad0cc1012a6eec38549f1ba17073a0
shayan balaee <shayanfall79@gmail.com>
2020-10-25 20:00:20 +0330

    Sitemap config for a few urls

--- src/http/controllers/homeController.js
+++ src/http/controllers/homeController.js
@@ -1,7 +1,13 @@
 const controller = require('./controller');
 const Course = require('src/models/course');
+const Episode = require('src/models/episode');
 const Comment = require('src/models/comment');
 const validator = require('validator');
+const sm = require('sitemap')
+const { createGzip } = require('zlib')
+const { Readable } = require('stream')
+
+let sitemap;
 
 class homeController extends controller {
   index = async (req, res) => {
@@ -46,6 +52,49 @@ class homeController extends controller {
       return false;
     }
   }
+
+  async sitemap(req, res, next) {
+    res.header('Content-Type', 'application/xml');
+    res.header('Content-Encoding', 'gzip');
+
+    if (sitemap) {
+      return res.send(sitemap);
+    }
+
+    try {
+      let smStream = new sm.SitemapStream({
+        hostname: process.env.WEBSITE_URL,
+        // cacheTime : 600000
+      });
+
+      let pipeline = smStream.pipe(createGzip());
+
+      smStream.write({ url: '/', changefreq: 'daily', priority: 1 })
+      smStream.write({ url: '/courses', priority: 1 })
+
+      let courses = await Course.find({}).sort({ createdAt: -1 }).exec();
+
+      courses.forEach(course => {
+        smStream.write({ url: course.path(), changefreq: 'weekly', priority: 0.8 })
+      })
+
+      let episodes = await Episode.find({}).populate('course').sort({ createdAt: -1 }).exec();
+
+      episodes.forEach(episode => {
+        smStream.write({ url: episode.path(), changefreq: 'weekly', priority: 0.8 })
+      })
+
+      sm.streamToPromise(pipeline).then(sm => sitemap = sm)
+
+      smStream.end()
+
+      pipeline.pipe(res).on('error', e => { throw e })
+
+    } catch (error) {
+      res.send(error)
+    }
+  }
+
 }
 
 module.exports = new homeController();
