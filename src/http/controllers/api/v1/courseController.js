b5649382788c142a42613d145857bbf8e02b4002
shayan balaee <shayanfall79@gmail.com>
2020-10-30 23:40:34 +0330

    more data filtering

--- src/http/controllers/api/v1/courseController.js
+++ src/http/controllers/api/v1/courseController.js
@@ -2,6 +2,7 @@ const controller = require('src/http/controllers/api/controller');
 const Course = require('src/models/course');
 const Episode = require('src/models/episode');
 const comment = require('src/models/comment');
+const passport = require('passport');
 
 
 class CourseController extends controller {
@@ -68,6 +69,10 @@ class CourseController extends controller {
                             sort: { number: 1 },
                         },
                     },
+                    {
+                        path: 'categories',
+                        select: 'name slug'
+                    }
                 ]);
 
             if (!course) {
@@ -77,17 +82,57 @@ class CourseController extends controller {
                 })
             }
 
-            res.json({
-                data: course,
-                status: 'success'
-            })
+            passport.authenticate('jwt', { session: false }, (err, user, info) => {
+                res.json({
+                    data: this.filterCourseData(course, user),
+                    status: 'success'
+                })
 
+            })(req, res);
 
         } catch (error) {
             res.status(500).json({
                 data: error.message,
                 status: 'error'
             })
+        }
+    }
+
+    filterCourseData(course, user) {
+        return {
+            id: course.id,
+            title: course.title,
+            slug: course.slug,
+            body: course.body,
+            image: course.thumb,
+            categories: course.categories.map(cate => {
+                return {
+                    name: cate.name,
+                    slug: cate.slug
+                }
+            }),
+            user: {
+                id: course.user.id,
+                name: course.user.name
+            },
+            episodes: course.episodes.map(episode => {
+                return {
+                    time: episode.time,
+                    downloadCount: episode.downloadCount,
+                    viewCount: episode.viewCount,
+                    commentCount: episode.commentCount,
+                    id: episode.id,
+                    title: episode.title,
+                    body: episode.body,
+                    type: episode.type,
+                    number: episode.number,
+                    createdAt: episode.createdAt,
+                    download: episode.download(!!user, user)
+                }
+            }),
+            price: course.price,
+            createdAt: course.createdAt
+
         }
     }
 
